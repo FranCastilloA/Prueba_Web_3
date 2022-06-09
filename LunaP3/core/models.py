0a5b8d70482b330e27f8a8ec5c374c9311fc1034
FranCastilloA <fr.castilloa@duocuc.cl>
2022-06-08 21:44:51 -0400

    actualizacion del modelo de base de datos

--- LunaP3/core/models.py
+++ LunaP3/core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+#Modelo base datos
+
+#modelo para Categoria
+
+class Categoria(models.Model):
+    idCategoria = models.IntegerField(primary_key=True, verbose_name='Id de categoria')
+    nombreCategoria = models.CharField(max_length=50, verbose_name='Nombre de la categoria')
+
+    def __str__(self):
+        return self.idCategoria
+
+#Modelo para Producto
+
+class Producto(models.Model):
+
+    sku = models.IntegerField(primary_key=True, verbose_name='SKU')
+    nombreProducto = models.CharField(max_length=50, verbose_name='Nombre de Producto')
+    precio = models.IntegerField(null=False, blank=False, verbose_name='Precio Producto')
+    stock = models.IntegerField(null=False, blank=False,verbose_name='Stock Producto')
+    descripcion = models.CharField(max_length=50, verbose_name='Descripcion Producto')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.sku
+
