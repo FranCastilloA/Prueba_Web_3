44d24a65401a5e2ade89c48cd8db9a1015215a7d
CrispoCL <cris.gonh@gmail.com>
2022-06-08 23:36:34 -0400

    Actualizacion estructura proyecto
    carpeta deprecated, urls y views

--- LunaP3/core/urls.py
+++ LunaP3/core/urls.py
@@ -1,19 +1,10 @@
 from django.urls import path
-from .views import index, nosotros, catalogo, catalogo2, formulario, producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8
+from .views import index, nosotros, catalogo, formulario
 
 urlpatterns = [
     path('',index,name='index'),
     path('index.html',index,name='index'),
     path('Nosotros.html',nosotros,name='nosotros'),
     path('Catalogo.html',catalogo,name='catalogo'),
-    path('Catalogo2.html',catalogo2,name='catalogo2'),
     path('Formulario.html',formulario,name='formulario'),
-    path('Producto1.html',producto1,name='producto1'),
-    path('Producto2.html',producto2,name='producto2'),
-    path('Producto3.html',producto3,name='producto3'),
-    path('Producto4.html',producto4,name='producto4'),
-    path('Producto5.html',producto5,name='producto5'),
-    path('Producto6.html',producto6,name='producto6'),
-    path('Producto7.html',producto7,name='producto7'),
-    path('Producto8.html',producto8,name='producto8'),
 ]
--- LunaP3/core/views.py
+++ LunaP3/core/views.py
@@ -19,42 +19,8 @@ def catalogo(request):
     
     return render(request, 'core/Catalogo.html')
 
-def catalogo2(request):
-    
-    return render(request, 'core/Catalogo2.html')
 
 def formulario(request):
     
     return render(request, 'core/Formulario.html')
 
-def producto1(request):
-    
-    return render(request, 'core/Producto1.html')
-
-def producto2(request):
-    
-    return render(request, 'core/Producto2.html')
-
-def producto3(request):
-    
-    return render(request, 'core/Producto3.html')
-
-def producto4(request):
-    
-    return render(request, 'core/Producto4.html')
-
-def producto5(request):
-    
-    return render(request, 'core/Producto5.html')
-
-def producto6(request):
-    
-    return render(request, 'core/Producto6.html')
-
-def producto7(request):
-    
-    return render(request, 'core/Producto7.html')
-
-def producto8(request):
-    
-    return render(request, 'core/Producto8.html')
